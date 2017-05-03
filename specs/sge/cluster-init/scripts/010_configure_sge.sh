3cf7e72cb5a58eab912376e2f4b09da06ad9f758
bwatrous <bwatrous@cyclecomputing.com>
2017-05-03 14:07:04 -0400

    don't overwrite sge configs set up by Cyclecloud - just set the gpu settings

--- specs/sge/cluster-init/scripts/010_configure_sge.sh
+++ specs/sge/cluster-init/scripts/010_configure_sge.sh
@@ -5,16 +5,29 @@ source /etc/cluster-setup.sh
 mkdir -p /etc/sge
 chmod 755 /etc/sge
 
+# Runs on all hosts
+cp ${CYCLECLOUD_SPEC_PATH}/files/prolog.sh /etc/sge/
+cp ${CYCLECLOUD_SPEC_PATH}/files/epilog.sh /etc/sge/
+chmod +x /etc/sge/prolog.sh
+chmod +x /etc/sge/epilog.sh
+
 
 # Runs only on scheduler
 if jetpack config roles | grep -q 'scheduler'; then
 
     # Configure GPU complex for SGE
-    qconf -Mc ${CYCLECLOUD_SPEC_PATH}/files/gpucomplex
-    qconf -Mconf ${CYCLECLOUD_SPEC_PATH}/files/global
+    qconf -sc > /tmp/gpucomplex
+    sed -i '/^#-----*/a gpu                 gpu          INT         <=      YES         YES        0        0' /tmp/gpucomplex
+    qconf -Mc /tmp/gpucomplex
+
+    # Create a prolog and epilog to consume gpus
+    qconf -sconf global > /tmp/global
+    sed -i 's|^prolog.*|prolog                       sgeadmin@/etc/sge/prolog.sh|' /tmp/global
+    sed -i 's|^epilog.*|epilog                       sgeadmin@/etc/sge/epilog.sh|' /tmp/global
+    qconf -Mconf /tmp/global
     
 else
-    # Runs only on exec nodes
+# Runs only on exec nodes
     
     # Install temporary cron job to update GPU info after node has been authorized by SGE
     cp ${CYCLECLOUD_SPEC_PATH}/files/modify_gpu_count.cron.sh /etc/sge
@@ -25,8 +38,3 @@ else
     
 fi
     
-# Runs on all hosts
-cp ${CYCLECLOUD_SPEC_PATH}/files/prolog.sh /etc/sge/
-cp ${CYCLECLOUD_SPEC_PATH}/files/epilog.sh /etc/sge/
-chmod +x /etc/sge/prolog.sh
-chmod +x /etc/sge/epilog.sh
